5a01787c71194d7b6e696824a269520c662764a1
HorvLala <horvathwebdev@gmail.com>
2023-05-01 20:57:54 +0200

    second commit

--- script.js
+++ script.js
@@ -1,73 +1,135 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+// ///////////////////////////////////////
+// // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.btn--close-modal');
+// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+// const openModal = function (e) {
+//   e.preventDefault();
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+// btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
-// creating and inserting elements
-// .insertAdjacentHTML
+// // creating and inserting elements
+// // .insertAdjacentHTML
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML =
-  'We use cookied for improved functionality an analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'We use cookied for improved functionality an analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-header.append(message);
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.parentElement.removeChild(message);
-  });
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// header.append(message);
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.parentElement.removeChild(message);
+//   });
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
 
-console.log(getComputedStyle(message).height);
-console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message).color);
 
-message.style.height = Number.parseFloat(getComputedStyle(message).
-height, 10 ) + 40 + 'px';
-console.log(getComputedStyle(message).height);
+// message.style.height = Number.parseFloat(getComputedStyle(message).
+// height, 10 ) + 40 + 'px';
+// console.log(getComputedStyle(message).height);
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+// // attributtes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+
+// logo.alt = 'Beautiful minimalist logo'
+// // non standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+
+// logo.setAttribute('company', 'Bankist');
+
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
+
+//   console.log(e.target.getBoundingClientRect());
+//   console.log('Curren scroll (x,y)', window.pageXOffset, window.pageYOffset);
+//   console.log(
+//   document.documentElement.clientHeight,
+//   document.documentElement.clientWidth
+
+//     );
+
+//     // window.scrollTo(
+//     //   s1coords.left + window.pageXOffset,
+//     //   s1coords.top + window.pageYOffset);
+
+//       window.scrollTo({
+//         left: s1coords.left + window.pageXOffset,
+//         top: s1coords.top + window.pageYOffset,
+//         behavior: 'smooth'
+//       });
+
+// });
+
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great, de ez constans');
+
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+  
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+
+// h1.addEventListener('mouseenter', function(e) {
+//   alert('addEventListener: Great')
+// });
+
+// h1.onmouseenter = function (e) {
+//   alert('Onmuse: Great')
+// }
+
+188
