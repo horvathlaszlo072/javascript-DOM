5205bae0c1b61e018054f6490098f026d968b61a
HorvLala <horvathwebdev@gmail.com>
2023-05-02 15:29:51 +0200

    sulibol az 191-ig

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 'use strict';
 
-// ///////////////////////////////////////
-// // Modal window
-
 // const modal = document.querySelector('.modal');
 // const overlay = document.querySelector('.overlay');
 // const btnCloseModal = document.querySelector('.btn--close-modal');
 // const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// ///////////////////////////////////////
+// // Modal window
+// ///////////////////////////////////////
 
 // const openModal = function (e) {
 //   e.preventDefault();
@@ -27,6 +30,52 @@
 // document.addEventListener('keydown', function (e) {
 //   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
 //     closeModal();
+//   }
+// });
+
+// /// Button scrolling
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
+
+//   console.log(e.target.getBoundingClientRect());
+//   console.log('Curren scroll (x,y)', window.pageXOffset, window.pageYOffset);
+//   console.log(
+//     document.documentElement.clientHeight,
+//     document.documentElement.clientWidth
+//   );
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // });
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
+
+//////////////////////////////////////////////
+///////////////// Page navigation ////////////
+//////////////////////////////////////////////
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   e.preventDefault();
+
+//   if (e.target.classList.contains('nav__link')) {
+//     const id = e.target.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
 //   }
 // });
 
@@ -84,33 +133,6 @@
 
 // logo.setAttribute('company', 'Bankist');
 
-// const btnScrollTo = document.querySelector('.btn--scroll-to');
-// const section1 = document.querySelector('#section--1');
-
-// btnScrollTo.addEventListener('click', function (e) {
-//   const s1coords = section1.getBoundingClientRect();
-//   console.log(s1coords);
-
-//   console.log(e.target.getBoundingClientRect());
-//   console.log('Curren scroll (x,y)', window.pageXOffset, window.pageYOffset);
-//   console.log(
-//   document.documentElement.clientHeight,
-//   document.documentElement.clientWidth
-
-//     );
-
-//     // window.scrollTo(
-//     //   s1coords.left + window.pageXOffset,
-//     //   s1coords.top + window.pageYOffset);
-
-//       window.scrollTo({
-//         left: s1coords.left + window.pageXOffset,
-//         top: s1coords.top + window.pageYOffset,
-//         behavior: 'smooth'
-//       });
-
-// });
-
 // const h1 = document.querySelector('h1');
 
 // const alertH1 = function (e) {
@@ -120,9 +142,7 @@
 
 // h1.addEventListener('mouseenter', alertH1);
 
-  
 // setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
-
 
 // h1.addEventListener('mouseenter', function(e) {
 //   alert('addEventListener: Great')
@@ -132,4 +152,41 @@
 //   alert('Onmuse: Great')
 // }
 
-188
+/////////////////////////// 188 /////////////////
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+// const i=randomColor()
+// document.querySelector('.nav__link').addEventListener
+// ('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav__links').addEventListener
+// ('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav').addEventListener
+// ('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// const h1 = document.querySelector('h1');
+
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = "White"
+// h1.lastElementChild.style.color = "black"
+
+// // going upwards : parents
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = "var(--gradient-secondary)"
+// h1.closest('h1').style.background = "var(--gradient-primary)"
+ 191
